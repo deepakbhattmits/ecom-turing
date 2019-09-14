685898ec2df27df23ab4563c6be9c9c88e21e2a8
deepakbhattmits <deepakbhattmits@gmail.com>
2019-09-14 12:43:21 +0530

    redux persit

--- src/index.js
+++ src/index.js
@@ -3,17 +3,36 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import reduxThunk from 'redux-thunk';
 import { createStore, applyMiddleware, compose } from 'redux';
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import App from './components/App';
 import reducers from './reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(
-    reducers,
-    composeEnhancers( applyMiddleware( reduxThunk ) ),
-    );
+
+const persistConfig = {
+    key: 'root',
+    storage,
+  }
+   
+  const persistedReducer = persistReducer(persistConfig, reducers); // new added line
+
+  const store = createStore(
+        persistedReducer,
+        composeEnhancers( applyMiddleware( reduxThunk ) ),
+    ); // new added line
+  const persistor = persistStore(store); // new added line
+  
+// const store = createStore(
+//     reducers,
+//     composeEnhancers( applyMiddleware( reduxThunk ) ),
+//     );
 ReactDOM.render(
             <Provider store={ store }>
-                <App />
+                <PersistGate loading={null} persistor={persistor}>
+                    <App />
+                </PersistGate>
             </Provider>
                 ,
                  document.querySelector('#root') 
