d9504284ea23a3c4f46aef110711fba824aed82c
deepak bhatt <deepakbhattmits@gmail.com>
2019-11-18 23:11:25 +0530

    search bar esc effect and close icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,89 +3,110 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { Menu } from 'semantic-ui-react';
 import Navigation from './ecom/Navigation';
-import { searchQuery } from '../actions' 
+import { searchQuery } from '../actions'
 class Header extends Component {
- 
-    state = { cartCount: 0 }
-     queryString = ( e ) => {
-         console.log('test : ',e.target.value);
-         this.props.searchQuery( e.target.value);
-     }
-     select = () => {
-    // this.props.selectedProd 
+
+    state = { cartCount: 0, value: '' }
+    queryString = (e) => {
+        console.log('test : ', e.target.value);
+
+        this.setState({ value: e.target.value }, () => { this.props.searchQuery(this.state.value); })
     }
-     renderResults = () => {
-             if(!this.props.searchResult.rows) {
-             return <div></div>;
-         }
-    
-         // const imgURL = require(`../assets/product_images`);
-        return this.props.searchResult.rows.map(( result, i ) => {
+    select = () => {
+        // this.props.selectedProd 
+    }
+    renderResults = () => {
+        if (!this.props.searchResult.rows) {
+            return <div>Loading...</div>;
+        }
+        return this.props.searchResult.rows.map((result, i) => {
             return (
-                    <Link to={`/ecom/prodDetails/${ result.product_id }`} className="result" key={ i } id={ result.product_id} onClick={this.select}>
-                        <div className="image">
-                            <img src={ require(`../assets/product_images/${ result.thumbnail }`) } alt={ result.thumbnail } />
-                        </div>
-                        <div className="content">
-                        <div className="price">{ result.discounted_price }</div>
-                        <div className="title">{ result.name }</div>
-                        <div className="description">{ `${result.description.substring(0, 35)}...` }</div>
-                        </div>
-                    </Link>
+                <Link to={`/ecom/prodDetails/${result.product_id}`} className="result" key={i} id={result.product_id} onClick={this.select}>
+                    <div className="image">
+                        <img src={require(`../assets/product_images/${result.thumbnail}`)} alt={result.thumbnail} />
+                    </div>
+                    <div className="content">
+                        <div className="price">{result.discounted_price}</div>
+                        <div className="title">{result.name}</div>
+                        <div className="description">{`${result.description.substring(0, 35)}...`}</div>
+                    </div>
+                </Link>
             );
         });
-     }
-    render () {
-         console.log(this.props.selProdData)
-        return (
-            <header className="ui header">  
-               <Menu >
-               <Menu.Item className="left menu">
-                   <Link to={`/`} >
-                        <span className="ui item" style={{ backgroundColor: '#f62f5e', color: '#fff', fontWeight: 'bolder' }} >
-                        S H O P M A T E
-                        </span>
-                    </Link>
-                </Menu.Item>
-                
-                <Navigation /> 
+    }
+    clearInput = () => {
+        this.setState({ value: '' }, () => { this.props.searchQuery(this.state.value); })
+    }
+    callback = e => {
+        console.log('TEST : ', e.keyCode)
+        if (e.keyCode === 27) {
+            this.setState({ value: '' }, () => { this.props.searchQuery(this.state.value); })
+        }
+    }
 
-                <Menu.Item className="right menu">
-                    <div className={`ui search category ${ this.props.searchResult.rows ? 'active visible': ''}`}>
-                        <div className="ui icon input">
-                            <input 
-                                className="prompt" 
-                                type="text" 
-                                placeholder="Search..." 
-                                onChange={ this.queryString } />
-                            <i className="search icon"></i>
-                        </div>
-                        <div className={`${this.props.searchResult.rows ? 'results transition visible': ''}`}>
-                         
-                            { this.renderResults() }
-                          
-                        </div>
-                    </div>
-                    <div className="bag">
-                        <Link to={`/ecom/cartPage`} >
-                            <p>{ this.state.cartCount }</p>
-                            <p><i className='icon shop'></i></p>
+    componentDidMount() {
+        window.addEventListener('keydown', this.callback, false);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('keydown', window.callback, false);
+    }
+
+    render() {
+        console.log(this.props.selProdData)
+        return (
+            <header className="ui header">
+                <Menu >
+                    <Menu.Item className="left menu">
+                        <Link to={`/`} >
+                            <span className="ui item" style={{ backgroundColor: '#f62f5e', color: '#fff', fontWeight: 'bolder' }} >
+                                S H O P M A T E
+                        </span>
                         </Link>
-                    </div>
                     </Menu.Item>
-                    
-               
+
+                    <Navigation />
+
+                    <Menu.Item className="right menu">
+                        <div className={`ui search category ${this.props.searchResult.rows ? 'active visible' : ''}`}>
+                            <div className="ui icon input">
+
+                                <input
+                                    className="prompt"
+                                    type="text"
+                                    placeholder="Search..."
+                                    onChange={this.queryString}
+                                    value={this.state.value}
+                                />
+                            </div>
+                            {this.state.value.length === 0 ? <i className="search icon"></i> : <i className="icon window close outline" onClick={this.clearInput}></i>}
+
+                            <div className={`results transition ${this.props.searchResult.rows.length > 0 ? 'visible' : 'hidden'}`}>
+
+                                {this.renderResults()}
+
+                            </div>
+                        </div>
+                        <div className="bag">
+                            <Link to={`/ecom/cartPage`} >
+                                <p>{this.state.cartCount}</p>
+                                <p><i className='icon shop'></i></p>
+                            </Link>
+                        </div>
+                    </Menu.Item>
+
+
                 </Menu>
-              
+
             </header>
         );
     }
 };
-const mapStateToProps = ( state ) => {
+const mapStateToProps = (state) => {
     return {
-         searchResult: state.ecomdata.searchResult,
-         selectedProd: state.ecomdata.selectedProdData,
+        searchResult: state.ecomdata.searchResult,
+        selectedProd: state.ecomdata.selectedProdData,
     }
 };
 
-export default connect(mapStateToProps,{ searchQuery })(Header);
+export default connect(mapStateToProps, { searchQuery })(Header);
