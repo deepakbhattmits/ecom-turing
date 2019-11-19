1996c0cd64713176cc6613918d4e8a0052a2e98b
deepak bhatt <deepakbhattmits@gmail.com>
2019-11-19 23:50:42 +0530

    added arrow keys naivation in result

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component, createRef } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { Menu } from 'semantic-ui-react';
 import Navigation from './ecom/Navigation';
 import { searchQuery } from '../actions'
+// import ListExample from './ListExample';
 class Header extends Component {
-    // constructor(props) {
-    //     super(props);
-    // this.ref = createRef();
-    // }
+    constructor(props) {
+        super(props);
+        this.myRef = createRef();
+    }
 
     state = { cartCount: 0, value: '' }
     queryString = (e) => {
@@ -19,13 +20,25 @@ class Header extends Component {
     select = () => {
         // this.props.selectedProd 
     }
+    moveFocus() {
+        const node = this.myRef.current;
+        node.addEventListener('keydown', function (e) {
+            const active = document.activeElement;
+            if (e.keyCode === 40 && active.nextSibling) {
+                active.nextSibling.focus();
+            }
+            if (e.keyCode === 38 && active.previousSibling) {
+                active.previousSibling.focus();
+            }
+        });
+    }
     renderResults = () => {
         if (!this.props.searchResult.rows) {
             return <div>Loading...</div>;
         }
         return this.props.searchResult.rows.map((result, i) => {
             return (
-                <Link to={`/ecom/prodDetails/${result.product_id}`} className="result" key={i} id={result.product_id} onClick={this.select}>
+                <Link to={`/ecom/prodDetails/${result.product_id}`} className="result" tabIndex={i} key={i} id={result.product_id} onClick={this.select}>
                     <div className="image">
                         <img src={require(`../assets/product_images/${result.thumbnail}`)} alt={result.thumbnail} />
                     </div>
@@ -46,12 +59,16 @@ class Header extends Component {
         if (e.keyCode === 27) {
             this.setState({ value: '' }, () => { this.props.searchQuery(this.state.value); })
         }
+        if (e.keyCode === 30) {
+            this.props.searchQuery(this.state.value);
+        }
     }
 
 
     componentDidMount() {
         window.addEventListener('keydown', this.callback, false);
         document.addEventListener('mousedown', this.handleClickOutside);
+        this.moveFocus();
     }
 
     componentWillUnmount() {
@@ -81,6 +98,8 @@ class Header extends Component {
         console.log(this.props.selProdData)
         return (
             <header className="ui header">
+
+                {/* <ListExample /> */}
                 <Menu >
                     <Menu.Item className="left menu">
                         <Link to={`/`} >
@@ -106,7 +125,7 @@ class Header extends Component {
                             </div>
                             {this.state.value.length === 0 ? <i className="search icon"></i> : <i className="icon window close outline" onClick={this.clearInput}></i>}
 
-                            <div className={`results transition ${this.props.searchResult.rows.length > 0 ? 'visible' : 'hidden'}`}>
+                            <div className={`results transition ${this.props.searchResult.rows.length > 0 ? 'visible' : 'hidden'}`} ref={this.myRef}>
 
                                 {this.renderResults()}
 
---
+++ src/components/ListExample.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class ListExample extends Component {
+    constructor(props) {
+        super(props);
+        this.myRef = React.createRef();
+    }
+    componentDidMount() {
+        this.moveFocus();
+    }
+    moveFocus() {
+        const node = this.myRef.current;
+        node.addEventListener('keydown', function (e) {
+            const active = document.activeElement;
+            if (e.keyCode === 40 && active.nextSibling) {
+                active.nextSibling.focus();
+            }
+            if (e.keyCode === 38 && active.previousSibling) {
+                active.previousSibling.focus();
+            }
+        });
+    }
+    render() {
+        return (
+            <div ref={this.myRef}>
+                <div tabIndex="0">First</div>
+                <div tabIndex="1">Second</div>
+                <div tabIndex="2">Third</div>
+            </div>
+        )
+    }
+}
+export default ListExample;
