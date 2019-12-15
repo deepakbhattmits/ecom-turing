cdd3df0b78090ab9d526aff972a1668d0aed2680
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-15 09:35:38 +0530

    refector

--- src/components/ecom/CategoryPage.jsx
+++ src/components/ecom/CategoryPage.jsx
@@ -1,52 +1,47 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { 
-        fetchCategoryProd,
-        fetchCategoriesData,
-        fetchAttributeSize,
-        fetchAttributeColor
-     } from '../../actions';
+import {
+  fetchCategoryProd,
+  fetchCategoriesData,
+  fetchAttributeSize,
+  fetchAttributeColor
+} from '../../actions';
 import ProductPage from './ProductPage';
 
 class CategoryPage extends Component {
-    componentDidMount () {
-        this.props.fetchCategoryProd( this.props.match.params.id );
-        this.props.fetchCategoriesData( this.props.match.params.id );
-        this.props.fetchAttributeSize();
-        this.props.fetchAttributeColor();
-    }
-    render () {
-        // console.log("test 0",this.props);
-        const catId = this.props.ecomSelCat.selCat.category_id;
-        const catName = this.props.ecomSelCat.selCat.name;
-        const catDescription = this.props.ecomSelCat.selCat.description;
-        return (
-            <div className="ui">
-              
-                    <h2 className="ui center aligned header">
-                    
-                        { catName }
-                    </h2>
-                    <p>{ catDescription } </p>
-               
-                 <ProductPage catId={ catId } /> 
-             
-            </div>
-        );
-    }
-};
-const mapDispatchToProps = ( dispatch ) => ({
-    fetchAttributeSize: () => dispatch(fetchAttributeSize()),
-    fetchAttributeColor: () => dispatch(fetchAttributeColor()),
-    fetchCategoryProd: ( data ) => dispatch(fetchCategoryProd( data ) ),
-    fetchCategoriesData: ( data ) => dispatch(fetchCategoriesData( data ) ),
-})
-const mapStateToProps = ( state, ownProps ) => {
-    // console.log("ownprops",ownProps);
-    return {
-        ecomdata: state.ecomdata[ownProps.match.params.id],
-        ecomSelCat: state.ecomSelCat,
-    }
+  componentDidMount() {
+    this.props.fetchCategoryProd(this.props.match.params.id);
+    this.props.fetchCategoriesData(this.props.match.params.id);
+    this.props.fetchAttributeSize();
+    this.props.fetchAttributeColor();
+  }
+  render() {
+    // console.log("test 0",this.props);
+    const catId = this.props.ecomSelCat.selCat.category_id;
+    const catName = this.props.ecomSelCat.selCat.name;
+    const catDescription = this.props.ecomSelCat.selCat.description;
+    return (
+      <div className='ui'>
+        <h2 className='ui center aligned header'>{catName}</h2>
+        <p>{catDescription} </p>
+
+        <ProductPage catId={catId} />
+      </div>
+    );
+  }
 }
-// fetchCategoriesData 
-export default connect(mapStateToProps,mapDispatchToProps)(CategoryPage);
+const mapDispatchToProps = dispatch => ({
+  fetchAttributeSize: () => dispatch(fetchAttributeSize()),
+  fetchAttributeColor: () => dispatch(fetchAttributeColor()),
+  fetchCategoryProd: data => dispatch(fetchCategoryProd(data)),
+  fetchCategoriesData: data => dispatch(fetchCategoriesData(data))
+});
+const mapStateToProps = (state, ownProps) => {
+  // console.log("ownprops",ownProps);
+  return {
+    ecomdata: state.ecomdata[ownProps.match.params.id],
+    ecomSelCat: state.ecomSelCat
+  };
+};
+// fetchCategoriesData
+export default connect(mapStateToProps, mapDispatchToProps)(CategoryPage);
