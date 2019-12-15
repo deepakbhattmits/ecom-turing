7e6848493a754bc80aab8530a83bfd0303dbe5ce
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-15 09:35:59 +0530

    refector

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import { createPortal } from 'react-dom';
 
-const Modal = (props) => {
-    return createPortal (
-        <div onClick={ props.onDismiss } className={`ui dimmer page  ${props.status ? 'visible active' : 'hidden'}`}>
-         <div onClick={ ( e ) => { e.stopPropagation() } } className={`ui text modal transition scrolling  ${props.status ? 'visible active' : 'hidden'}`}>
-            <i className="close icon" onClick={ props.onDismiss}></i>
-            <div className="header">{ props.title }</div>
-            <div className="content">{ props.content }</div>
-         </div>
-        </div>
+const Modal = props => {
+  return createPortal(
+    <div
+      onClick={props.onDismiss}
+      className={`ui dimmer page  ${
+        props.status ? 'visible active' : 'hidden'
+      }`}
+    >
+      <div
+        onClick={e => {
+          e.stopPropagation();
+        }}
+        className={`ui text modal transition scrolling  ${
+          props.status ? 'visible active' : 'hidden'
+        }`}
+      >
+        <i className='close icon' onClick={props.onDismiss}></i>
+        <div className='header'>{props.title}</div>
+        <div className='content'>{props.content}</div>
+      </div>
+    </div>,
 
-    ,document.querySelector('#modal')
-    )
+    document.querySelector('#modal')
+  );
 };
 export default Modal;
