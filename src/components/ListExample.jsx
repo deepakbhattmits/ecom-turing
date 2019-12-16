e71ff20f5906ac3892be1c763ac4cb617d79e8bd
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-16 21:07:14 +0530

    refector

--- src/components/ListExample.jsx
+++ src/components/ListExample.jsx
@@ -3,13 +3,13 @@ import React, { Component } from 'react';
 class ListExample extends Component {
   constructor(props) {
     super(props);
-    this.myRef = React.createRef();
+    this.Ref = React.createRef();
   }
   componentDidMount() {
     this.moveFocus();
   }
   moveFocus() {
-    const node = this.myRef.current;
+    const node = this.Ref.current;
     node.addEventListener('keydown', function(e) {
       const active = document.activeElement;
       if (e.keyCode === 40 && active.nextSibling) {
@@ -22,7 +22,7 @@ class ListExample extends Component {
   }
   render() {
     return (
-      <div ref={this.myRef}>
+      <div ref={this.Ref}>
         <div tabIndex='0'>First</div>
         <div tabIndex='1'>Second</div>
         <div tabIndex='2'>Third</div>
