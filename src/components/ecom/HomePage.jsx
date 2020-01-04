934da12abb10facb1ae52d03a174a7b8da40fdcd
deepak bhatt <deepakbhattmits@gmail.com>
2020-01-04 10:52:31 +0530

    refector

--- src/components/ecom/HomePage.jsx
+++ src/components/ecom/HomePage.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class HomePage extends Component {
   render() {
-    console.log(this.props);
+    // console.log(this.props);
     return <div>Homepage !</div>;
   }
 }
