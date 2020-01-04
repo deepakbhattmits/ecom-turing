50f9b3ea1a02d3c14b3ddc5dd69d7a94f6ab1746
deepak bhatt <deepakbhattmits@gmail.com>
2020-01-04 10:37:23 +0530

    refector

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 const Footer = () => {
-  return <footer className='ui footer '></footer>;
+  return <footer className='ui footer '>
+  i am footer</footer>;
 };
 export default Footer;
