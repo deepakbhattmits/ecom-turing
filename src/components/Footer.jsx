0be6cec40ba78afee394f65907a85bdf83d388eb
deepak bhatt <deepakbhattmits@gmail.com>
2020-01-05 10:16:01 +0530

    new changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 const Footer = () => {
-  return <footer className='ui footer '>
-  i am footer</footer>;
+  // console.log('TEST')
+  return (<footer className='ui footer '>
+  i am footer</footer>);
 };
 export default Footer;
