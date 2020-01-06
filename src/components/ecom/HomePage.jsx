9715b8a93f40a9f9e29549554d5ad0c342f23494
deepak bhatt <deepakbhattmits@gmail.com>
2020-01-06 22:20:55 +0530

    added home page

--- src/components/ecom/HomePage.jsx
+++ src/components/ecom/HomePage.jsx
@@ -1,9 +1,11 @@
+/** @format */
+
 import React, { Component } from 'react';
 
 class HomePage extends Component {
-  render() {
-    // console.log('TEST :',this.props);
-    return <div>Homepage !</div>;
-  }
+	render() {
+		// console.log('TEST:',this.props);
+		return <div>Homepage !</div>;
+	}
 }
 export default HomePage;
