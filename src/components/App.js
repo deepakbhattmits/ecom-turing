db8d23d6d7d18fda612fdd810f90c9f4c645aee0
deepak bhatt <deepakbhattmits@gmail.com>
2020-01-06 22:18:54 +0530

    added app

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,5 @@
+/** @format */
+
 import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 import createBrowserHistory from '../history';
@@ -9,24 +11,30 @@ import CartPage from './ecom/CartPage';
 import ProdDetails from './ecom/ProdDetails';
 import ReviewPage from './ecom/ReviewPage';
 const App = () => {
-  // console.log('TEST : ');
-  return (
-    <div className='ui grid center aligned'>
-      <div className='ui fifteen wide column'>
-        <Router history={createBrowserHistory}>
-          <Header />
-          <Switch>
-            <Route path='/' exact component={MainPage} />
-            <Route path='/ecom/categories/:id' component={CategoryPage} />
-            <Route path='/ecom/cartPage' component={CartPage} />
-            <Route path='/ecom/prodDetails/:id' component={ProdDetails} />
-            <Route path='/ecom/ReviewPage/:id' component={ReviewPage} />
-          </Switch>
-          <Footer />
-        </Router>
-      </div>
-    </div>
-  );
+	// console.log('TEST :> ');
+	return (
+		<div className='ui grid center aligned'>
+			<div className='ui fifteen wide column'>
+				<Router history={createBrowserHistory}>
+					<div className='ui header'>
+						<Header />
+					</div>
+					<main className='ui content-body'>
+						<Switch>
+							<Route path='/' exact component={MainPage} />
+							<Route path='/ecom/categories/:id' component={CategoryPage} />
+							<Route path='/ecom/cartPage' component={CartPage} />
+							<Route path='/ecom/prodDetails/:id' component={ProdDetails} />
+							<Route path='/ecom/ReviewPage/:id' component={ReviewPage} />
+						</Switch>
+					</main>
+					<div className='ui footer'>
+						<Footer />
+					</div>
+				</Router>
+			</div>
+		</div>
+	);
 };
 
 export default App;
