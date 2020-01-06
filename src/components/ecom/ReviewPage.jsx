ec9cc00aac8c57a3f34f6225d4126e1e41d1057d
deepak bhatt <deepakbhattmits@gmail.com>
2020-01-06 22:18:23 +0530

    refector

--- src/components/ecom/ReviewPage.jsx
+++ src/components/ecom/ReviewPage.jsx
@@ -1,24 +1,26 @@
+/** @format */
+
 import React, { Component } from 'react';
 import _ from 'lodash';
 
 class ReviewPage extends Component {
-  render() {
-    // console.log(this.props)
-    return (
-      <div className='ui relaxed divided list'>
-        <div className='item'>
-          <div className='ui large star rating'>
-            {_.times(this.props.ratings, i => (
-              <i className='icon yellow' key={i} />
-            ))}
-          </div>
-          <div className='content '>
-            <span className='header'> {this.props.name}</span>
-            <div className='description'>Updated 10 mins ago</div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		// console.log('TEST :',this.props)
+		return (
+			<div className='ui relaxed divided list'>
+				<div className='item'>
+					<div className='ui large star rating'>
+						{_.times(this.props.ratings, i => (
+							<i className='icon yellow' key={i} />
+						))}
+					</div>
+					<div className='content '>
+						<span className='header'> {this.props.name}</span>
+						<div className='description'>Updated 10 mins ago</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 export default ReviewPage;
