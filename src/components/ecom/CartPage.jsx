dfebc4e665cad1f7c0b4369c07af164de60f7a79
deepak bhatt <deepakbhattmits@gmail.com>
2020-03-25 14:27:50 +0530

    refector

--- src/components/ecom/CartPage.jsx
+++ src/components/ecom/CartPage.jsx
@@ -1,25 +1,25 @@
+/** @format */
+
 import React, { useState } from 'react';
 import RainBow from '../hoc/RainBow';
 import PropTypes from 'prop-types';
 
-const CartPage =()=> {
-  const [inputVal, setInputVal] = useState('');
-  console.log('test : ')
-    return (
-      <div className='cart-page'>
-        cart count
-        <input
-          type='text'
-          onChange={e =>
-            setInputVal(e.target.value)
-                     }
-          value={inputVal}
-        />
-        {`Input Text is :${inputVal}`}
-      </div>
-    );
-  }
+const CartPage = () => {
+	const [inputVal, setInputVal] = useState('');
+	// console.log('test : ')
+	return (
+		<div className='cart-page'>
+			cart count
+			<input
+				type='text'
+				onChange={e => setInputVal(e.target.value)}
+				value={inputVal}
+			/>
+			{`Input Text is :${inputVal}`}
+		</div>
+	);
+};
 CartPage.propTypes = {
-  inputVal: PropTypes.string
+	inputVal: PropTypes.string
 };
 export default RainBow(CartPage);
