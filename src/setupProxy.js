3f2147af9ce7b237b112babad4ad35da10e022ba
deepak bhatt <deepakbhattmits@gmail.com>
2020-11-09 08:28:38 +0530

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,1 +1,6 @@
-const proxy = require('http-proxy-middleware'); module.exports = function(app) { app.use(proxy('/auth/google', { target: 'http://localhost:3003/' } )); }
+/** @format */
+
+const proxy = require('http-proxy-middleware');
+module.exports = function (app) {
+	app.use(proxy('/auth/google', { target: 'http://localhost:3003/' }));
+};
