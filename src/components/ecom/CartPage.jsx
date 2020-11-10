8f9e855bcae81c863fb311a27ffcba05c5e0e095
deepak bhatt <deepakbhattmits@gmail.com>
2020-11-10 08:22:26 +0530

    Update CartPage.jsx

--- src/components/ecom/CartPage.jsx
+++ src/components/ecom/CartPage.jsx
@@ -2,13 +2,11 @@
 import RainBow from '../hoc/RainBow';
 import PropTypes from 'prop-types';
 
-const CartPage = () => {
-	return (
-		<div className='cart-page'>
-			<h1>cart </h1>
-		</div>
-	);
-};
+const CartPage = () => (
+	<div className='cart-page'>
+		<h1>cart </h1>
+	</div>
+);
 CartPage.propTypes = {
 	inputVal: PropTypes.string,
 };
