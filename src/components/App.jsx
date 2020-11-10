493433875f3eee4f11c2c03d5256650c6a6357ab
deepak bhatt <deepakbhattmits@gmail.com>
2020-11-10 08:22:06 +0530

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 /** @format */
+import { useState, useEffect } from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 import createBrowserHistory from '../history';
 import CategoryPage from './ecom/CategoryPage';
@@ -9,7 +10,8 @@ import CartPage from './ecom/CartPage';
 import ProdDetails from './ecom/ProdDetails';
 import ReviewPage from './ecom/ReviewPage';
 const App = () => {
-	// console.log('TEST :> ');
+	// console.log('TEST : > ');
+
 	return (
 		<div className='ui grid center aligned'>
 			<div className='ui fifteen wide column'>
@@ -17,6 +19,7 @@ const App = () => {
 					<div className='ui header'>
 						<Header />
 					</div>
+
 					<main className='ui content-body'>
 						<Switch>
 							<Route path='/' exact component={MainPage} />
