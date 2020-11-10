0b24a35d2b2fdc3c004f86ccc3bf1cdd45fa6cbf
deepak bhatt <deepakbhattmits@gmail.com>
2020-11-10 08:22:16 +0530

    Update MainPage.jsx

--- src/components/ecom/MainPage.jsx
+++ src/components/ecom/MainPage.jsx
@@ -29,6 +29,31 @@ const MainPage = (props) => {
 	// }
 	return (
 		<>
+			<section>
+				<article className='post'>
+					<h1>The Principles of Good Design</h1>
+					<p className='post-meta'>
+						Circa 2014 <span>by</span> Ian Daniel Stewart
+					</p>
+
+					<p>
+						Is <strong>beauty</strong> objectively true? Are there principles
+						you can use to guide you to it in your work? Can designers from both
+						the sciences and the arts look at each others work and find{' '}
+						<strong className='sevenhundred'>beauty</strong>? These are
+						important questions for all designers.
+					</p>
+
+					<p>
+						For those of us who design things … we need to be able to recognize
+						it. We need good taste to make good things. Instead of treating{' '}
+						<strong className='eighthundred'>beauty</strong> as an airy
+						abstraction, to be either blathered about or avoided depending on
+						how one feels about airy abstractions, let’s try considering it as a
+						practical question: how do you make good stuff?
+					</p>
+				</article>
+			</section>
 			<div className='ui vertical stripe segment inverted'>
 				<div className='ui middle aligned stackable grid container'>
 					<div className='row'>
