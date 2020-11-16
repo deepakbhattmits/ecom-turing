1bf7e4977424c844546f1264300bfc27643aadfa
deepak bhatt <deepakbhattmits@gmail.com>
2020-11-16 14:38:23 +0530

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
+/** @format */
+
 import { combineReducers } from 'redux';
 import ecomReducer from './ecomReducer';
-
+// ecomReducer
 export default combineReducers({
-    ecomdata: ecomReducer,
-    ecomSelCat: ecomReducer,
-})
+	ecomdata: ecomReducer,
+	ecomSelCat: ecomReducer,
+});
