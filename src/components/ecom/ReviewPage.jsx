8c0194f8f254f002d9907c29e5d23896d6509423
deepak bhatt <deepakbhattmits@gmail.com>
2020-11-26 00:03:41 +0530

    Update ReviewPage.jsx

--- src/components/ecom/ReviewPage.jsx
+++ src/components/ecom/ReviewPage.jsx
@@ -1,25 +1,20 @@
 /** @format */
-import { Component } from 'react';
 import _ from 'lodash';
-// TODO:functional ReviewPage
-class ReviewPage extends Component {
-	render() {
-		// console.log('TEST : ', this.props);
-		return (
-			<div className='ui relaxed divided list'>
-				<div className='item'>
-					<div className='ui large star rating'>
-						{_.times(this.props.ratings, (i) => (
-							<i className='icon yellow' key={i} />
-						))}
-					</div>
-					<div className='content '>
-						<span className='header'> {this.props.name}</span>
-						<div className='description'>Updated 10 mins ago</div>
-					</div>
-				</div>
+// ReviewPage
+const ReviewPage = ({ ratings, name }) => (
+	<div className='ui relaxed divided list'>
+		<div className='item'>
+			<div className='ui large star rating'>
+				{_.times(ratings, (i) => (
+					<i className='icon yellow' key={i} />
+				))}
 			</div>
-		);
-	}
-}
+			<div className='content '>
+				<span className='header'> {name}</span>
+				<div className='description'>Updated 10 mins ago</div>
+			</div>
+		</div>
+	</div>
+);
+// }
 export default ReviewPage;
