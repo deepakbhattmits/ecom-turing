eb2d3a4b33c9528eb22046fb5a3af5d6e323847c
deepak bhatt <deepakbhattmits@gmail.com>
2020-12-10 00:03:12 +0530

    Create DatePicker.jsx

---
+++ src/components/reusable/DatePicker.jsx
@@ -0,0 +1,36 @@
+/** @format */
+
+import { useEffect, useRef } from 'react';
+
+function useListener(ref, eventName, handler) {
+	useEffect(() => {
+		if (ref.current) {
+			const element = ref.current;
+			element.addEventListener(eventName, handler);
+			return () => element.removeEventListener(eventName, handler);
+		}
+	}, [eventName, handler, ref]);
+}
+
+const DatePicker = ({
+	onChange,
+	onFocus,
+	onBlur,
+	dateAdapter,
+	localization,
+	...props
+}) => {
+	const ref = useRef(null);
+
+	useListener(ref, 'duetChange', onChange);
+	useListener(ref, 'duetFocus', onFocus);
+	useListener(ref, 'duetBlur', onBlur);
+
+	useEffect(() => {
+		ref.current.localization = localization;
+		ref.current.dateAdapter = dateAdapter;
+	}, [localization, dateAdapter]);
+
+	return <duet-date-picker ref={ref} {...props}></duet-date-picker>;
+};
+export default DatePicker;
