a279056585e78c28791f338c6b81ec4da70abd20
deepak bhatt <deepakbhattmits@gmail.com>
2020-12-11 21:34:25 +0530

    Update CartPage.jsx

--- src/components/ecom/CartPage.jsx
+++ src/components/ecom/CartPage.jsx
@@ -1,12 +1,20 @@
 /** @format */
 import RainBow from '../hoc/RainBow';
 import PropTypes from 'prop-types';
-
-const CartPage = () => (
-	<div className='cart-page'>
-		<h1>cart.</h1>
-	</div>
-);
+import DatePicker from '../reusable/DatePicker';
+const CartPage = () => {
+	// const [date, setDate] = useState();
+	return (
+		<div className='cart-page'>
+			<h1>cart</h1>
+			<DatePicker
+				identifier='date'
+				value='any'
+				onChange={(e) => console.log(e.detail)}
+			/>
+		</div>
+	);
+};
 CartPage.propTypes = {
 	inputVal: PropTypes.string,
 };
