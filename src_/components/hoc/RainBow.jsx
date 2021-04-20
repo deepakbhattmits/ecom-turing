5dc55ae8ac600123e72165b0225ae6067f1f66b3
deepak bhatt <deepakbhattmits@gmail.com>
2021-04-20 09:37:57 +0530

    Create RainBow.jsx

---
+++ src_/components/hoc/RainBow.jsx
@@ -0,0 +1,18 @@
+/** @format */
+//  RainBow
+const RainBow = (WrappedComponent) => {
+	const colours = ['red', 'blue', 'green', 'pink', 'cyan'];
+	const randomColour = colours[Math.floor(Math.random() * 4)];
+	const textColour = '#fff';
+	const className = randomColour + '--text';
+	return (props) => {
+		return (
+			<div
+				className={`ui section ${className}`}
+				style={{ backgroundColor: `${randomColour}`, color: `${textColour}` }}>
+				<WrappedComponent {...props} />
+			</div>
+		);
+	};
+};
+export default RainBow;
