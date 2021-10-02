5aa1c6f542ea08c8478dfad0aa38ee10519fe72a
deepakbhattmits <deepakbhattmits@gmail.com>
2021-10-02 07:17:07 +0530

    Create Navigation.jsx

---
+++ src__/components/ecom/Navigation.jsx
@@ -0,0 +1,122 @@
+/** @format */
+
+import { useState, useEffect } from 'react';
+import { connect, useSelector } from 'react-redux';
+import history from '../../history';
+import {
+	fetchDepartments,
+	fetchCategories,
+	fetchCategoriesData,
+	fetchCategoryProd,
+} from '../../actions';
+
+const Navigation = (props) => {
+	//  console.log(' TEST   : ');
+	const departments = useSelector((state) => state.ecomdata.departments);
+	const [visible, setVisible] = useState(false);
+	const [active, setActive] = useState(false);
+	useEffect(() => {
+		if (!departments.length) {
+			props.fetchDepartments();
+		}
+	}, [props, departments]);
+	const departmentClick = (e) => {
+		props.fetchCategories(e.target.id);
+		setVisible(!visible);
+		setActive(!active);
+	};
+	const cat = (e) => {
+		const { id } = e.target;
+		history.push(`/ecom/categories/${id}`);
+		setVisible(false);
+		props.fetchCategoriesData(id);
+		props.fetchCategoryProd(id);
+	};
+	const renderDepartments = () => {
+		if (Object.values(departments).length === 0) {
+			return <div>Loading...</div>;
+		}
+		return departments.map(({ name, department_id }) => {
+			return (
+				<div
+					role='listbox'
+					aria-expanded='false'
+					className='ui pointing dropdown link item '
+					key={department_id}
+					id={department_id}
+					onClick={departmentClick}>
+					<div
+						className={`text ${
+							Object.values(props.categories).length > 0
+								? props.categories.some(
+										(item) => item.department_id === department_id
+								  )
+									? active
+										? 'active'
+										: ''
+									: ''
+								: ''
+						}`}
+						role='alert'
+						aria-live='polite'
+						id={department_id}
+						onClick={departmentClick}>
+						{' '}
+						{name}
+					</div>
+					<i
+						aria-hidden='true'
+						className='dropdown icon'
+						id={department_id}
+						onClick={departmentClick}
+					/>
+
+					<div
+						className={`menu transition ${
+							Object.values(props.categories).length > 0
+								? props.categories.some(
+										(item) => item.department_id === department_id
+								  )
+									? visible
+										? 'visible'
+										: ''
+									: ''
+								: ''
+						}`}>
+						{renderCategories()}
+					</div>
+				</div>
+			);
+		});
+	};
+	const renderCategories = () => {
+		if (Object.values(props.categories).length === 0) {
+			return <div>Loading...</div>;
+		}
+		return props.categories.map(({ category_id, name, department_id }) => {
+			return (
+				<div className='item' id={category_id} key={category_id} onClick={cat}>
+					{name}
+				</div>
+			);
+		});
+	};
+	return (
+		<>
+			<div className='ui menu'>{renderDepartments()}</div>
+		</>
+	);
+};
+const mapDispatchToProps = (dispatch) => ({
+	fetchDepartments: () => dispatch(fetchDepartments()),
+	fetchCategories: (data) => dispatch(fetchCategories(data)),
+	fetchCategoriesData: (data) => dispatch(fetchCategoriesData(data)),
+	fetchCategoryProd: (data) => dispatch(fetchCategoryProd(data)),
+});
+const mapStateToProps = (state) => {
+	return {
+		categories: state.ecomdata.departmentCategory,
+		departments: state.ecomdata.departments,
+	};
+};
+export default connect(mapStateToProps, mapDispatchToProps)(Navigation);
