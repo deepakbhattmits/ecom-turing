a71bfd114866f5da2f959518a12578d19348c18c
deepakbhattmits <deepakbhattmits@gmail.com>
2021-11-11 08:33:44 +0530

    Create ReviewPage.jsx

---
+++ src_/components/ecom/ReviewPage.jsx
@@ -0,0 +1,20 @@
+/** @format */
+import _ from 'lodash';
+// ReviewPage
+const ReviewPage = ({ ratings, name }) => (
+	<div className='ui relaxed divided list'>
+		<div className='item'>
+			<div className='ui large star rating'>
+				{_.times(ratings, (i) => (
+					<i className='icon yellow' key={i} />
+				))}
+			</div>
+			<div className='content '>
+				<span className='header'> {name}</span>
+				<div className='description'>Updated 10 mins ago</div>
+			</div>
+		</div>
+	</div>
+);
+// }
+export default ReviewPage;
