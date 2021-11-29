5cc731bd57a3534d5f83fa89291134bcc6f95c6a
deepakbhattmits <deepakbhattmits@gmail.com>
2021-11-29 07:43:29 +0530

    Delete index.js

--- src_/reducers/index.js
+++
@@ -1,9 +0,0 @@
-/** @format */
-
-import { combineReducers } from 'redux';
-import ecomReducer from './ecomReducer';
-//  ecomReducer
-export default combineReducers({
-	ecomdata: ecomReducer,
-	ecomSelCat: ecomReducer,
-});
